dc79b7d0a59ff45b707b0c670548e595aadf6a3e
idstackdev <id.stackdevelopers@gmail.com>
2024-07-11 15:08:09 +0700

    "Added latest posts feature to blog page, refactored popular posts section, and updated template to display post data dynamically"

--- src/app/Http/Controllers/HomePage/BlogPageController.js
+++ src/app/Http/Controllers/HomePage/BlogPageController.js
@@ -3,8 +3,15 @@ const Category = require("../../../Models/Categories"); // Pastikan path ke mode
 
 exports.index = async (req, res) => {
   try {
+    // Mengambil data dari database, mengurutkannya berdasarkan tanggal pembuatan terbaru, dan membatasi jumlah data menjadi 9
+    const latestPosts = await Post.find()
+      .sort({ createdAt: -1 })
+      .limit(9)
+      .populate("category_id", "name")
+      .exec();
+
     // Mengambil data dari database, mengurutkannya berdasarkan views tertinggi, dan membatasi jumlah data menjadi 4
-    const posts = await Post.find()
+    const popularPosts = await Post.find()
       .sort({ views: -1 })
       .limit(4)
       .populate("category_id", "name")
@@ -13,7 +20,8 @@ exports.index = async (req, res) => {
     // Render data ke template view
     res.render("homepage/blog/blog", {
       title: "Blog",
-      posts: posts, // Kirimkan data post ke view
+      latestPosts: latestPosts, // Kirimkan data post terbaru ke view
+      popularPosts: popularPosts, // Kirimkan data post populer ke view
     });
   } catch (err) {
     res.status(500).json({ message: err.message });
