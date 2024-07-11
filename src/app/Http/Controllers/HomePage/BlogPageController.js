0bb3cc363e648dbfe44d29ff79560621adadba38
idstackdev <id.stackdevelopers@gmail.com>
2024-07-11 15:10:30 +0700

    "Added categories retrieval and display in BlogPageController and blog.ejs template"

--- src/app/Http/Controllers/HomePage/BlogPageController.js
+++ src/app/Http/Controllers/HomePage/BlogPageController.js
@@ -3,14 +3,17 @@ const Category = require("../../../Models/Categories"); // Pastikan path ke mode
 
 exports.index = async (req, res) => {
   try {
-    // Mengambil data dari database, mengurutkannya berdasarkan tanggal pembuatan terbaru, dan membatasi jumlah data menjadi 9
+    // Mengambil data kategori
+    const categories = await Category.find().exec();
+
+    // Mengambil data post terbaru, diurutkan berdasarkan tanggal pembuatan terbaru, dan dibatasi jumlahnya menjadi 9
     const latestPosts = await Post.find()
       .sort({ createdAt: -1 })
       .limit(9)
       .populate("category_id", "name")
       .exec();
 
-    // Mengambil data dari database, mengurutkannya berdasarkan views tertinggi, dan membatasi jumlah data menjadi 4
+    // Mengambil data post populer, diurutkan berdasarkan views tertinggi, dan dibatasi jumlahnya menjadi 4
     const popularPosts = await Post.find()
       .sort({ views: -1 })
       .limit(4)
@@ -20,6 +23,7 @@ exports.index = async (req, res) => {
     // Render data ke template view
     res.render("homepage/blog/blog", {
       title: "Blog",
+      categories: categories, // Kirimkan data kategori ke view
       latestPosts: latestPosts, // Kirimkan data post terbaru ke view
       popularPosts: popularPosts, // Kirimkan data post populer ke view
     });
